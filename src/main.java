49b2606507b771d06b2c75749ca94f18b56fc606
ayokunle adeleye <adeways2000@gmail.com>
2021-05-05 10:01:16 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println(NumberOfDaysInMonth.isLeapYear(-1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2007));
+        System.out.println(NumberOfDaysInMonth.isLeapYear(2000));
+
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2018));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
+    }
+}
